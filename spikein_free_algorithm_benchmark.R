a631542c12567fe6b6ef359fac5b7ce13b101390
XIAOJIE QIU <xqiu@trapnell-head.grid.gs.washington.edu>
2015-11-11 16:40:07 -0800

    update the spikein_free_algorithm_benchmark function

--- spikein_free_algorithm_benchmark.R
+++ spikein_free_algorithm_benchmark.R
@@ -1,3 +1,9 @@
+load('prepare_lung_data.RData')
+  library(monocle)
+  library(xacHelper)
+
+  load_all_libraries()
+
 ############################make the landscape heatmap: 
 mc_select <- coef(rlm(unlist(lapply(molModels_select, function(x) coef(x)[1])) ~ unlist(lapply(molModels_select, function(x) coef(x)[2]))))
 
@@ -14,9 +20,12 @@ optim_mc_func_fix_c_test_optim(x_list[[1]])
 optimization_landscape_3d <- lapply(X = split(expand.grid(c(seq(-6, -1, length.out = 100), -4.403166,  as.numeric(mc_select[2])), 
                    c(seq(0, 4, length.out = 100), 2.77514, as.numeric(mc_select[1]))), 1:102^2), optim_mc_func_fix_c_test_optim)
 
-#this doesn't work, need to use roster and sp: 
-optimization_landscape_2d <- melt(optimization_landscape_3d)
-qplot(Var1, Var2, fill=optim_res, geom="tile", data=optimization_landscape_2d) + scale_fill_gradientn(colours=rainbow(7)) #hmcols
-ggsave(filename = paste(elife_directory, 'eLife_fig4E.pdf', sep = ''), width = 1.38, height = 1.25)
+# #this doesn't work, need to use roster and sp: 
+# optimization_landscape_2d <- melt(optimization_landscape_3d)
 
-save.image()
+# pdf('eLife_fig4E.pdf')
+# qplot(Var1, Var2, fill=optim_res, geom="tile", data=optimization_landscape_2d) + scale_fill_gradientn(colours=rainbow(7)) #hmcols
+# # ggsave(filename = paste(elife_directory, 'eLife_fig4E.pdf', sep = ''), width = 1.38, height = 1.25)
+# dev.off()
+
+save.image('spikein_free_algorithm_benchmark.RData')
